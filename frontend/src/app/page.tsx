6f87ff46bfc994e3273dbaf4173218cb461faef2
kavimugilsk <147433527+Kavisk46@users.noreply.github.com>
2026-07-09 01:20:01 +0530

    Initialsetup

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>OpsMind AI</h1>
+      <p>Enterprise Knowledge Intelligence Platform</p>
+    </main>
+  );
+}
---
+++ frontend/src/components/Sidebar/sidebar.tsx
@@ -0,0 +1,19 @@
+export default function Sidebar() {
+  return (
+    <aside>
+      <h2>OpsMind AI</h2>
+
+      <ul>
+        <li>🏠 Dashboard</li>
+        <li>📄 Documents</li>
+        <li>🤖 AI Assistant</li>
+        <li>🔍 Search</li>
+        <li>👥 Teams</li>
+        <li>📊 Analytics</li>
+        <li>⚙️ Settings</li>
+      </ul>
+    </aside>
+  );
+}
+
+ 
